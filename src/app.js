4a2985b79f11b44f840abebbe4e3eb4bceb1d9e7
denizu9 <denizural9@gmail.com>
2023-06-06 22:57:36 +0300

    javascript first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log("Merhaba")
+
+let dolarYesterday = 20
+let dolarToday = 20.1
+
+console.log(dolarYesterday)
+
+const euroYesterday = 22.11 // constant variable, because keyword, data type  is const
+
+let houseCredits = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Kanut Kredisi"]
+
+console.log("<ul>")
+for(let index = 0; index < houseCredits.length; index++){
+    console.log("<li>" + houseCredits[index] + "</li>")
+}
+
+console.log("</ul>")
+
+
