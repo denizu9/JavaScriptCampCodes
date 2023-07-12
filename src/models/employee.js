8626b46d8f8462fc5228789fa380b442e5611fe7
denizu9 <denizural9@gmail.com>
2023-07-12 21:19:06 +0300

    new commit

--- src/models/employee.js
+++ src/models/employee.js
@@ -1,5 +1,6 @@
 import User from "./user.js";
 
+
 export default class Employee extends User{
     constructor(id,firstName, lastName, city, age, salary){
         super(id,firstName,lastName,city,age)
