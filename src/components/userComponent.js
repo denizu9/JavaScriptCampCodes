39069507fc1917d74fb02c71a9bb94c882bc913b
denizu9 <denizural9@gmail.com>
2023-07-13 10:35:36 +0300

    new commit

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -7,8 +7,8 @@ console.log("User component loaded!")
 let logger1 = new MongoLogger()
 let userService = new UserService(logger1)
 
-let user1 = new User(1,"Deniz", "Ural", "İstanbul")
-let user2 = new User(2,"İrem", "Kadıoğlu","Giresun")
+let user1 = new User(1,"Deniz", "Ural", "İstanbul",25)
+let user2 = new User(2,"İrem", "Kadıoğlu","Giresun",24)
 
 userService.add(user1)
 userService.add(user2)
@@ -16,3 +16,8 @@ userService.add(user2)
 console.log(userService.list())
 
 console.log(userService.getById(2))
+
+userService.load()
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
